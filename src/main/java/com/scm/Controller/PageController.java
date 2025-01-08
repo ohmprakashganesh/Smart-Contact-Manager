764032fe33b989b10faac3c442942341fbea2554
ohmprakashganesh <131688194+ohmprakashganesh@users.noreply.github.com>
2025-01-08 14:51:52 +0545

    nav bar designing

--- src/main/java/com/scm/Controller/PageController.java
+++ src/main/java/com/scm/Controller/PageController.java
@@ -11,19 +11,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
+
     @RequestMapping("/home")
-    public String home(Model model){
-        System.out.println("home page is here ");
-        model.addAttribute("name", "Om prakash ganesh" );
-        model.addAttribute("age", "55");
-        return "home";
-
-    }
-
-    @RequestMapping("/about")
     public String  aboutPage(){
         System.out.println("about page is initialized");
+        return "home";
+    }
+    @RequestMapping("/about")
+    public String  aboutPage2(){
+        System.out.println("about page is initialized");
         return "about";
+    }
+    @RequestMapping("/services")
+    public String  aboutPage3(){
+        System.out.println("about page is initialized");
+        return "services";
+    }
+
+    @RequestMapping("/contact")
+    public String  contactpage(){
+        System.out.println("about page is initialized");
+        return "contact";
+    }
+
+    @RequestMapping("/login")
+    public String  loginPage(){
+        System.out.println("about page is initialized");
+        return "login";
+    }
+
+    @RequestMapping("/signup")
+    public String  signUp(){
+        System.out.println("about page is initialized");
+        return "register";
     }
 
     
